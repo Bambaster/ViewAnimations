cb9ac3a2ee2ad9ad5084222618ef19fada39f982
Vladimir <popovvs86@gmail.com>
2015-03-09 17:42:16 +0300

    Initial Commit

---
+++ ViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewTest
+//
+//  Created by Lowtrack on 09.03.15.
+//  Copyright (c) 2015 Vladimir Popov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewTest
+//
+//  Created by Lowtrack on 09.03.15.
+//  Copyright (c) 2015 Vladimir Popov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
